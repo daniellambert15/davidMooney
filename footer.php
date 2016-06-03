e4f926900f68304b239247ac1006960fb90893a4
supergizmo <Daniel@dlssystems.co.uk>
2016-06-03 09:52:46 +0100

    Finished product

---
+++ footer.php
@@ -0,0 +1,32 @@
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+		<div class="col-xs-12">
+				<div class="container striketrhough whiteBG">
+						<p class="seperator"></p>
+				</div>
+		</div>
+
+		<div class="col-xs-12">
+			<p>
+				<strong>David Mooney</strong><br />
+				5 Chapel View<br />
+				Station Road<br />
+				Turton<br />
+				Bolton BL7 0LE<br />
+				Tel: 07887 552 442
+			</p>
+		</div>
+
+		<div class="col-xs-12">
+				<p>
+						<a href="terms.php">Terms and Conditions</a>
+				</p>
+		</div>
+	</body>
+</html>
