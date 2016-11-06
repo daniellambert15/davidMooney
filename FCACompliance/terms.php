d7490ac95965cc430f6bb77162732a2bd0933c15
daniel <daniel@rawrsome.co.uk>
2016-11-06 19:11:56 +0000

    adding fca

---
+++ FCACompliance/terms.php
@@ -0,0 +1,26 @@
+<?php include 'head.php'; ?>
+		<div class="col-xs-12">
+			<h2>TERMS AND CONDITIONS OF USE OF FCA TRAINING PRESENTATION AND ITS CONTENT</h2>
+			<p>
+It is a regulatory requirement to have staff who are competent and trained and for each firm that holds a consumer credit license to comply with the TC module of the FCA Handbook.</p>
+
+<p>Firms have an obligation to ensure that staff are knowledgeable about and have an understanding of the FCA, their regulatory framework and the Handbook regulations and requirements. Knowing how and why the FCA regulates a firm is just as important as every other training area – so it can be evidenced that staff are trained about the FCA.</p>
+
+<p>David Mooney (the Author) provides this training presentation and its content as part of the ongoing services on behalf of FCA regulated firms and may from time to time allow individuals and firms (the Viewers) to view its contents as part of their training obligations.</p>
+
+<p>This FCA training presentation has been produced in order to evidence that training has been provided to staff members of a firm and may not be used, sold on, reproduced or publicly displayed or distributed to any other company, organisation or person for any commercial or public purpose without the express written permission of the Author. This includes loaning, leasing or renting the training questionnaire to any other third party.</p>
+
+<p>The Author will not be liable, under any circumstances and in any way, for any errors, omissions, loss, or damage of any kind incurred as a result of the use of the training questionnaire or the use of any content included therein.</p>
+
+<p>The Viewers must evaluate and bear all risks associated with the use of any content, including any reliance on the accuracy and completeness. The Author cannot guarantee successful outcomes with third parties (such as the FCA and the Financial Ombudsman Service) as such third parties can exercise their discretion in deciding particular matters.</p>
+
+<p>The copyright for the training questionnaire remains with the Author.</p>
+
+<p>The Viewers agree to indemnify and hold the Author harmless for any and all claims or demands, including reasonable solicitor’s fees that arise from or otherwise relate to its use of the training questionnaire.</p>
+
+<p>The Author is a provider of resources and guidance only and is not bound by the professional responsibilities and duties of a legal practitioner.</p>
+
+<p>Each party warrants, represents and undertakes to the other party that it shall comply with its respective obligations under the Data Protection Act and take appropriate and technical and organisational measures to protect those data against unauthorised or unlawful processing and against accidental loss, destruction, alteration or damage.
+</p>
+		</div><!-- /.col -->
+<?php include 'footer.php'; ?>
---
+++ FCACompliance/thankyou.php
@@ -0,0 +1,5 @@
+<?php include 'head.php'; ?>
+		<div class="col-xs-12">
+			<p>Thank you for completing our questionnaire. A member of our staff will give you a call back shortly.</p>
+		</div><!-- /.col -->
+<?php include 'footer.php'; ?>
